604ea13b242da29dc53211d24f4bcb59ff756aa5
Wooaje-jeong <woo6388@naver.com>
2025-04-08 16:05:25 +0900

    feat: 공통 ErrorType, BadRequestException 정의

---
+++ src/main/java/com/econo_4factorial/newproject/common/exception/ErrorType.java
@@ -0,0 +1,11 @@
+package com.econo_4factorial.newproject.common.exception;
+
+import org.springframework.http.HttpStatus;
+
+public interface ErrorType {
+    String getErrorCode ();
+
+    HttpStatus getHttpStatus();
+
+    String getMessage ();
+}
