719c302e16bbdb9eb7805c0ab8a415723af02642
xxbeann <yjb4945@gmail.com>
2025-05-15 06:26:48 +0900

    feat: AuthToken return값 추가

--- src/main/java/com/econo_4factorial/newproject/auth/jwt/AuthToken.java
+++ src/main/java/com/econo_4factorial/newproject/auth/jwt/AuthToken.java
@@ -5,4 +5,8 @@ public record AuthToken (
     String refreshToken,
     String grantType,
     Long expirationTime
-) {}
+) {
+    public static AuthToken of(String accessToken, String refreshToken, String tokenHeader, long accessTokenExpiredTime) {
+        return new AuthToken(accessToken, refreshToken, tokenHeader, accessTokenExpiredTime);
+    }
+}
