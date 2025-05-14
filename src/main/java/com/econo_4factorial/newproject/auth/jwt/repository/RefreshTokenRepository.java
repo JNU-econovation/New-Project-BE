9f5db1bec42df1a44cf24f3e22ee25202543af05
xxbeann <yjb4945@gmail.com>
2025-05-15 06:27:55 +0900

    feat: refreshTokeen 존재 확인하는 boolean값 추가

--- src/main/java/com/econo_4factorial/newproject/auth/jwt/repository/RefreshTokenRepository.java
+++ src/main/java/com/econo_4factorial/newproject/auth/jwt/repository/RefreshTokenRepository.java
@@ -4,4 +4,5 @@ import com.econo_4factorial.newproject.auth.jwt.RefreshToken;
 import org.springframework.data.repository.CrudRepository;
 
 public interface RefreshTokenRepository extends CrudRepository<RefreshToken, Long> {
+    boolean existsByRefreshToken(String refreshToken);
 }
