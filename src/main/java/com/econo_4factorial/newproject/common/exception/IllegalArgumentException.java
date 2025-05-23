57a19aaa4a5f184a90c3c50da3b523280c4dcfcb
Wooaje-jeong <woo6388@naver.com>
2025-05-23 21:27:50 +0900

    feat: IllegalArgumentException 구현

---
+++ src/main/java/com/econo_4factorial/newproject/common/exception/IllegalArgumentException.java
@@ -0,0 +1,7 @@
+package com.econo_4factorial.newproject.common.exception;
+
+public class IllegalArgumentException extends BadRequestException{
+    public IllegalArgumentException() {
+        super(CommonErrorType.ILLEGAL_ARGUMENT_EXCEPTION);
+    }
+}
