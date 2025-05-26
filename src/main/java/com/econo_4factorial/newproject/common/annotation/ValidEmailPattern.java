e9f0ade9fad84062a2815f796606e9edacdb5253
Wooaje-jeong <woo6388@naver.com>
2025-05-26 16:48:38 +0900

    feat: 이메일 유효성 검사를 수행하는 커스텀 어노테이션 구현

---
+++ src/main/java/com/econo_4factorial/newproject/common/annotation/ValidEmailPattern.java
@@ -0,0 +1,19 @@
+package com.econo_4factorial.newproject.common.annotation;
+
+import com.econo_4factorial.newproject.common.annotation.validator.ValidEmailPatternValidator;
+import com.econo_4factorial.newproject.common.exception.ValidationMessage;
+import jakarta.validation.Constraint;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = ValidEmailPatternValidator.class)
+public @interface ValidEmailPattern {
+    String message() default ValidationMessage.EMAIL_INVALID_PATTERN;
+    Class[] groups() default {};
+    Class[] payload() default {};
+}
