38a2b52aa7e968b5c8fc73cb76ef3d8e3ff75923
Wooaje-jeong <woo6388@naver.com>
2025-05-26 16:32:40 +0900

    feat: 애플 공개키 데이터 조회 HttpClient 구현

---
+++ src/main/java/com/econo_4factorial/newproject/auth/service/apple/AppleOAuthFeignClient.java
@@ -0,0 +1,11 @@
+package com.econo_4factorial.newproject.auth.service.apple;
+
+import com.econo_4factorial.newproject.auth.dto.apple.ApplePublicKeysResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@FeignClient(name="appleOAuthFeinghClient", url ="https://appleid.apple.com/auth")
+public interface AppleOAuthFeignClient {
+    @GetMapping("/keys")
+    ApplePublicKeysResponse getApplePublicKeys ();
+}
