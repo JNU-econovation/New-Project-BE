cf212e2048acec1e0c900a956e9f1b551cf4a610
Wooaje-jeong <woo6388@naver.com>
2025-05-26 16:32:40 +0900

    feat: 애플 공개키 데이터 조회 HttpClient 구현

---
+++ src/main/java/com/econo_4factorial/newproject/auth/dto/apple/ApplePublicKeysResponse.java
@@ -0,0 +1,16 @@
+package com.econo_4factorial.newproject.auth.dto.apple;
+
+import java.util.List;
+
+public record ApplePublicKeysResponse(
+        List<ApplePublicKey> keys
+) {
+    public record ApplePublicKey(
+        String kty,
+        String kid,
+        String use,
+        String alg,
+        String n,
+        String e
+    ) {}
+}
