02fbd5bfc845ce3706f35ba7d6f48e8ec3f7a7a2
Wooaje-jeong <woo6388@naver.com>
2025-05-26 16:12:03 +0900

    refactor: 카카오에서 제공하는 사용자 식별자 값도 받도록 DTO 리팩터링

--- src/main/java/com/econo_4factorial/newproject/auth/dto/kakao/KaKaoUserInfoRes.java
+++ src/main/java/com/econo_4factorial/newproject/auth/dto/kakao/KaKaoUserInfoRes.java
@@ -1,28 +1,25 @@
 package com.econo_4factorial.newproject.auth.dto.kakao;
 
-import com.econo_4factorial.newproject.user.dto.UserInfoDTO;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record KaKaoUserInfoRes(
+    Long id,
+    @JsonProperty("kakao_account")
     KakaoAccount kakaoAccount
 ) {
 
     public record KakaoAccount(
             String email,
-            String name,
-            String phoneNumber
+            String name
     ) {
-        public KakaoAccount (String email, String name, String phoneNumber) {
+        public KakaoAccount (String email, String name) {
             this.email = email;
             this.name = name;
-            this.phoneNumber = formatPhoneNumber(phoneNumber);
         }
 
-        private String formatPhoneNumber(String phoneNumber) {
-            return phoneNumber.replace("+82 ", "0");
-        }
     }
 
-    public UserInfoDTO toUserInfoDTO() {
-        return new UserInfoDTO(this.kakaoAccount.email, this.kakaoAccount.name, this.kakaoAccount.phoneNumber);
+    public KakaoUserInfoDTO toKaKaoUserInfoDTO() {
+        return new KakaoUserInfoDTO(this.id, this.kakaoAccount.email, this.kakaoAccount.name);
     }
 }
