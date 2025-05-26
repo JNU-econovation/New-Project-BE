ca1f0ea3c2f0d9318ed19aa8ce6a1704060d23e0
Wooaje-jeong <woo6388@naver.com>
2025-05-26 16:57:58 +0900

    feat: 카카오와 애플 로그인에 대한 사용자 식별자 추가 및 user 빌더 패턴 적용

--- src/main/java/com/econo_4factorial/newproject/user/domain/User.java
+++ src/main/java/com/econo_4factorial/newproject/user/domain/User.java
@@ -20,12 +20,25 @@ public class User extends BaseEntity {
     @AttributeOverrides({
             @AttributeOverride(name = "email", column = @Column(name = "email", nullable = false, unique = true)),
             @AttributeOverride(name = "name", column = @Column(name = "name", nullable=false)),
-            @AttributeOverride(name = "phoneNumber", column = @Column(name = "phone_number", nullable = false))
+            @AttributeOverride(name = "phoneNumber", column = @Column(name = "phone_number"))
     })
     private UserInfo userInfo;
 
-    @Builder
-    public User(String email, String name, String phoneNumber) {
-        this.userInfo = new UserInfo(email, name, phoneNumber);
+    @Column(unique = true)
+    private Long kakaoId;
+
+    @Column(unique = true)
+    private String appleSub;
+
+    @Builder(builderMethodName = "kakaoUserBuilder", builderClassName = "kakaoUserBuilder")
+    public User(String email, String name, Long kakaoId) {
+        this.userInfo = new UserInfo(email, name);
+        this.kakaoId = kakaoId;
+    }
+
+    @Builder(builderMethodName = "appleUserBuilder", builderClassName = "appleUserBuilder")
+    public User(String email, String name, String appleSub) {
+        this.userInfo = new UserInfo(email, name);
+        this.appleSub = appleSub;
     }
 }
