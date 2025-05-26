9713966e4438ec383e25d7a19d84a45ebd340dd7
Wooaje-jeong <woo6388@naver.com>
2025-05-26 16:49:12 +0900

    feat: 클라이언트 요청에 대한 유효성 검사 에러 메세지 정의

---
+++ src/main/java/com/econo_4factorial/newproject/common/exception/ValidationMessage.java
@@ -0,0 +1,9 @@
+package com.econo_4factorial.newproject.common.exception;
+
+public class ValidationMessage {
+    public static final String IDENTITY_TOKEN_REQUIRED = "identityToken이 누락되었습니다";
+    public static final String FAMILY_NAME_REQUIRED = "familyName이 누락되었습니다";
+    public static final String GIVEN_NAME_REQUIRED = "givenName이 누락되었습니다";
+    public static final String EMAIL_INVALID_PATTERN = "email이 올바르지 않은 패턴입니다";
+    public static final String REFRESH_TOKEN_REQUIRED = "refreshToken이 누락되었습니다";
+}
