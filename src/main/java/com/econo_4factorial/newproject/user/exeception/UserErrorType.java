2697b102d7e5a48d0ac94a3ea9b76c549ed44709
Wooaje-jeong <woo6388@naver.com>
2025-05-26 17:00:30 +0900

    feat: UserErrorType 구현

---
+++ src/main/java/com/econo_4factorial/newproject/user/exeception/UserErrorType.java
@@ -0,0 +1,33 @@
+package com.econo_4factorial.newproject.user.exeception;
+
+import com.econo_4factorial.newproject.common.exception.ErrorType;
+import org.springframework.http.HttpStatus;
+
+public enum UserErrorType implements ErrorType {
+    USER_NOT_FOUND_EXCEPTION ("User400_001", HttpStatus.NOT_FOUND, "회원을 찾을 수 없습니다");
+
+    private final String errorCode;
+    private final HttpStatus httpStatus;
+    private final String message;
+
+    UserErrorType(String errorCode, HttpStatus httpStatus, String message) {
+        this.errorCode = errorCode;
+        this.httpStatus = httpStatus;
+        this.message = message;
+    }
+
+    @Override
+    public String getErrorCode() {
+        return errorCode;
+    }
+
+    @Override
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+}
