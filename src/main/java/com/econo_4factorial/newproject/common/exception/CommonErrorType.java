e5e08eb2c937e9205e9a19831c14fce9f1a8a34b
Wooaje-jeong <woo6388@naver.com>
2025-05-31 21:41:15 +0900

    style: 세미콜론 삭제

--- src/main/java/com/econo_4factorial/newproject/common/exception/CommonErrorType.java
+++ src/main/java/com/econo_4factorial/newproject/common/exception/CommonErrorType.java
@@ -7,7 +7,7 @@ public enum CommonErrorType implements ErrorType{
     METHOD_ARGUMENT_NOT_VALID_EXCEPTION("COMMON400_001", HttpStatus.BAD_REQUEST, "RequestDTO 유효성 검사 미통과"),
     ILLEGAL_ARGUMENT_EXCEPTION("COMMON400_002", HttpStatus.BAD_REQUEST, "Illegal argument exception 발생 "),
     MISSING_PATH_VARIABLE_EXCEPTION("COMMON400_003", HttpStatus.BAD_REQUEST, "경로 변수(PathVariable)가 누락됐습니다."),
-    MISSING_REQUEST_PARAM_EXCEPTION("COMMON400_004", HttpStatus.BAD_REQUEST, "쿼리 스트링이 누락됐습니다.");
+    MISSING_REQUEST_PARAM_EXCEPTION("COMMON400_004", HttpStatus.BAD_REQUEST, "쿼리 스트링이 누락됐습니다."),
   
     UN_EXPECTED_EXCEPTION("Common500_001", HttpStatus.INTERNAL_SERVER_ERROR, "예기치 못한 에러가 발생했습니다.");
 
