b3e203721619b8b9cd14d6368354d1cb1bd31d56
xxbeann <yjb4945@gmail.com>
2025-07-05 03:25:20 +0900

    chore:MountainConroller swagger annotation 추가

--- src/main/java/com/econo_4factorial/newproject/mountain/controller/MountainController.java
+++ src/main/java/com/econo_4factorial/newproject/mountain/controller/MountainController.java
@@ -5,6 +5,8 @@ import com.econo_4factorial.newproject.common.util.api.ApiResult;
 import com.econo_4factorial.newproject.mountain.dto.MountainDTO;
 import com.econo_4factorial.newproject.mountain.dto.res.GetMountainsRes;
 import com.econo_4factorial.newproject.mountain.service.MountainService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,10 +18,12 @@ import java.util.List;
 @AllArgsConstructor
 @RestController
 @RequestMapping("/api/v1/mountains")
+@Tag(name = "Mountain", description = "산 관련 API")
 public class MountainController {
     private final MountainService mountainService;
 
     @GetMapping
+    @Operation(summary = "전체 산 목록 조회", description = "등록된 모든 산의 정보를 반환합니다.")
     public ApiResult<ApiResult.SuccessBody<GetMountainsRes>> getAllMountains() {
         List<MountainDTO> mountains = mountainService.findAll();
         return ApiResponse.success(GetMountainsRes.from(mountains), HttpStatus.OK);
