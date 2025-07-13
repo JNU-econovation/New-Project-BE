a67bb36e6959c4dc2d6fdfbfc2375aa9837575bc
Wooaje-jeong <woo6388@naver.com>
2025-07-14 00:34:55 +0900

    chore: userId 커스텀 어노테이션의 resolver 추가

--- src/main/java/com/econo_4factorial/newproject/common/config/WebConfig.java
+++ src/main/java/com/econo_4factorial/newproject/common/config/WebConfig.java
@@ -1,11 +1,19 @@
 package com.econo_4factorial.newproject.common.config;
 
+import com.econo_4factorial.newproject.common.annotation.resolver.UserIdResolver;
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 @Configuration
+@AllArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+    private final UserIdResolver userIdResolver;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
@@ -24,4 +32,10 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(userIdResolver);
+    }
+
 }
