b42f98916848a04110f819a5f451f24be8056afd
Wooaje-jeong <woo6388@naver.com>
2025-07-14 00:37:38 +0900

    feat: CourseService 구현

---
+++ src/main/java/com/econo_4factorial/newproject/course/service/CourseService.java
@@ -0,0 +1,22 @@
+package com.econo_4factorial.newproject.course.service;
+
+import com.econo_4factorial.newproject.course.dto.CourseSearchCondition;
+import com.econo_4factorial.newproject.course.dto.res.CourseDTO;
+import com.econo_4factorial.newproject.course.repository.CourseCustomRepository;
+import com.econo_4factorial.newproject.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CourseService {
+    private final CourseCustomRepository courseCustomRepository;
+    private final UserService userService;
+
+    public List<CourseDTO> getAllCoursesWithBookmark(Long userId, Long mountainId, String sortBy) {
+        CourseSearchCondition courseSearchCondition = CourseSearchCondition.of(mountainId, sortBy);
+        return courseCustomRepository.findAllByMountainIdWithBookmark(courseSearchCondition, mountainId, userId);
+    }
+}
