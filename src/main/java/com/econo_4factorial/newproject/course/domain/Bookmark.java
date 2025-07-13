3dc2917004048b60cc11b2f3be6f401d5314bcdc
Wooaje-jeong <woo6388@naver.com>
2025-07-14 00:39:55 +0900

    feat: bookmark 도메인 모델 구현

---
+++ src/main/java/com/econo_4factorial/newproject/course/domain/Bookmark.java
@@ -0,0 +1,24 @@
+package com.econo_4factorial.newproject.course.domain;
+
+import com.econo_4factorial.newproject.user.domain.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Bookmark {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn
+    private Course course;
+}
