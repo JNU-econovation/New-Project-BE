a20ed2d0cb5b75d0a79b61287a47390b64f2e20a
xxbeann <yjb4945@gmail.com>
2025-07-14 20:16:28 +0900

    feat: BaseRepository 생성

---
+++ src/main/java/com/econo_4factorial/newproject/base/repository/BaseRepository.java
@@ -0,0 +1,7 @@
+package com.econo_4factorial.newproject.base.repository;
+
+import com.econo_4factorial.newproject.base.domain.Base;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BaseRepository extends JpaRepository<Base, Long> {
+}
