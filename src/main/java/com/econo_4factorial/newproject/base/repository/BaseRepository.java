9b9fde03e2b6e813f732b15480f027dc7a4b8af2
xxbeann <yjb4945@gmail.com>
2025-07-14 20:17:05 +0900

    chore: 주석 추가

--- src/main/java/com/econo_4factorial/newproject/base/repository/BaseRepository.java
+++ src/main/java/com/econo_4factorial/newproject/base/repository/BaseRepository.java
@@ -4,4 +4,5 @@ import com.econo_4factorial.newproject.base.domain.Base;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BaseRepository extends JpaRepository<Base, Long> {
+    //추후 customRepository로 변경될수도
 }
