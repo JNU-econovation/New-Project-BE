2fd40f925058574897f83572bf67d33ec7f8b396
xxbeann <yjb4945@gmail.com>
2025-07-14 20:13:58 +0900

    feat: baseImage entity 추가

---
+++ src/main/java/com/econo_4factorial/newproject/base/domain/BaseImage.java
@@ -0,0 +1,22 @@
+package com.econo_4factorial.newproject.base.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class BaseImage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "base_id")
+    private Base base;
+
+    @Column(unique = true, nullable = false)
+    private String imageUrl;
+}
