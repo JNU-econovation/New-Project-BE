01c814e4f8a472215838c0f529a3606a354825b8
xxbeann <yjb4945@gmail.com>
2025-07-16 17:41:54 +0900

    feat: Bases 응답DTO 작성

---
+++ src/main/java/com/econo_4factorial/newproject/base/dto/res/GetBasesRes.java
@@ -0,0 +1,14 @@
+package com.econo_4factorial.newproject.base.dto.res;
+
+import com.econo_4factorial.newproject.base.dto.BaseDTO;
+
+import java.util.List;
+
+public record GetBasesRes(
+        Long mountainId,
+        List<BaseDTO> bases
+) {
+    public static GetBasesRes from(Long mountainId, List<BaseDTO> bases) {
+        return new GetBasesRes(mountainId,bases);
+    }
+}
