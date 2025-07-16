f8c55bf6127bd4a616ba6bedb54dfbb81673edba
xxbeann <yjb4945@gmail.com>
2025-07-16 22:54:12 +0900

    feat: GetFacilitiesRes 작성

---
+++ src/main/java/com/econo_4factorial/newproject/facility/dto/res/GetFacilitiesRes.java
@@ -0,0 +1,14 @@
+package com.econo_4factorial.newproject.facility.dto.res;
+
+import com.econo_4factorial.newproject.facility.dto.FacilityDTO;
+
+import java.util.List;
+
+public record GetFacilitiesRes(
+        Long mountainId,
+        List<FacilityDTO> facilities
+) {
+    public static GetFacilitiesRes from(Long mountainId, List<FacilityDTO> facilities) {
+        return new GetFacilitiesRes(mountainId, facilities);
+    }
+}
