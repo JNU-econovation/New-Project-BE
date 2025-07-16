49d8f0acb58948a80286eaa1f2768f63fdd616d7
Wooaje-jeong <woo6388@naver.com>
2025-07-16 17:50:13 +0900

    feat: Bookmark 컨트롤러 구현

---
+++ src/main/java/com/econo_4factorial/newproject/course/controller/BookmarkController.java
@@ -0,0 +1,25 @@
+package com.econo_4factorial.newproject.course.controller;
+
+import com.econo_4factorial.newproject.common.annotation.UserId;
+import com.econo_4factorial.newproject.common.util.api.ApiResponse;
+import com.econo_4factorial.newproject.common.util.api.ApiResult;
+import com.econo_4factorial.newproject.course.service.BookmarkService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/courses/bookmark")
+@AllArgsConstructor
+public class BookmarkController {
+    private final BookmarkService bookmarkService;
+
+    @PostMapping("/{courseId}")
+    public ApiResult<ApiResult.SuccessBody<Void>> addBookmark (
+            @UserId Long userId,
+            @PathVariable(name="courseId") Long courseId
+    ) {
+        bookmarkService.addBookmark(userId, courseId);
+        return ApiResponse.success(HttpStatus.CREATED);
+    }
+}
