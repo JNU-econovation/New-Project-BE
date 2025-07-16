bb77faf0a42c710b8cb837f0e3986cb63669d100
xxbeann <yjb4945@gmail.com>
2025-07-16 21:32:46 +0900

    feat: isMountainExistThrow 추가

--- src/main/java/com/econo_4factorial/newproject/mountain/service/MountainService.java
+++ src/main/java/com/econo_4factorial/newproject/mountain/service/MountainService.java
@@ -1,6 +1,7 @@
 package com.econo_4factorial.newproject.mountain.service;
 
 import com.econo_4factorial.newproject.mountain.dto.MountainDTO;
+import com.econo_4factorial.newproject.mountain.exception.BadRequestException.MountainNotFoundException;
 import com.econo_4factorial.newproject.mountain.repository.MountainRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -19,5 +20,12 @@ public class MountainService {
                 .stream()
                 .map(MountainDTO::from)
                 .toList();
+    }
+
+    @Transactional(readOnly = true)
+    public void isMountainExistThrow(Long mountainId) {
+        if (!mountainRepository.existsById(mountainId)) {
+            throw new MountainNotFoundException();
+        }
     }
 }
