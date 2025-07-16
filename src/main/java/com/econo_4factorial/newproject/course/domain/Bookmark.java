97e76a068935a55aaab3f36397e61d43f4f4c88e
Wooaje-jeong <woo6388@naver.com>
2025-07-16 17:49:34 +0900

    refactor: Bookmark에 Builder 패턴 적용

--- src/main/java/com/econo_4factorial/newproject/course/domain/Bookmark.java
+++ src/main/java/com/econo_4factorial/newproject/course/domain/Bookmark.java
@@ -2,12 +2,12 @@ package com.econo_4factorial.newproject.course.domain;
 
 import com.econo_4factorial.newproject.user.domain.User;
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
 @Getter
+@Builder
+@AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Bookmark {
     @Id
