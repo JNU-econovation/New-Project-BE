76f16d33912a00f03d5f105424977dd38e80f59f
xxbeann <yjb4945@gmail.com>
2025-07-16 17:40:46 +0900

    feat: 특정 산 id에 속한 base 리스트 조회

--- src/main/java/com/econo_4factorial/newproject/base/repository/BaseRepository.java
+++ src/main/java/com/econo_4factorial/newproject/base/repository/BaseRepository.java
@@ -3,6 +3,8 @@ package com.econo_4factorial.newproject.base.repository;
 import com.econo_4factorial.newproject.base.domain.Base;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BaseRepository extends JpaRepository<Base, Long> {
-    //추후 customRepository로 변경될수도
+    List<Base> findByMountainId(Long mountainId);
 }
