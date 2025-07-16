a879fc93ad038dbfd15acfc32f35a12c10b7577d
xxbeann <yjb4945@gmail.com>
2025-07-16 22:45:15 +0900

    feat: findByMountainId 메서드 추가

--- src/main/java/com/econo_4factorial/newproject/facility/repository/FacilityRepository.java
+++ src/main/java/com/econo_4factorial/newproject/facility/repository/FacilityRepository.java
@@ -3,5 +3,8 @@ package com.econo_4factorial.newproject.facility.repository;
 import com.econo_4factorial.newproject.facility.domain.Facility;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface FacilityRepository extends JpaRepository<Facility, Long> {
+    List<Facility> findByMountainId(Long mountainId);
 }
