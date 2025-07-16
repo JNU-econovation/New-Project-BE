a3703ebf2f2dcb432c8796e432f56d37de6f16ab
xxbeann <yjb4945@gmail.com>
2025-07-16 17:42:22 +0900

    feat: BaseController 작성
    산 id를 쿼리 파라미터로 받아 모든 거점으 조회하는 api를 작성했습니다.

---
+++ src/main/java/com/econo_4factorial/newproject/base/controller/BaseController.java
@@ -0,0 +1,29 @@
+package com.econo_4factorial.newproject.base.controller;
+
+import com.econo_4factorial.newproject.base.dto.BaseDTO;
+import com.econo_4factorial.newproject.base.dto.res.GetBasesRes;
+import com.econo_4factorial.newproject.base.service.BaseService;
+import com.econo_4factorial.newproject.common.util.api.ApiResponse;
+import com.econo_4factorial.newproject.common.util.api.ApiResult;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/v1/bases")
+@Tag(name = "Base", description = "거점 관련 API")
+public class BaseController {
+    private final BaseService baseService;
+
+    @GetMapping
+    @Operation(summary = "전체 거점 목록 조회", description = "해당 산의 모든 거점 위도, 경도를 반환합니다.")
+    public ApiResult<ApiResult.SuccessBody<GetBasesRes>> getBasesByMountain(@RequestParam Long mountainId) {
+        List<BaseDTO> baseDTOS = baseService.getBasesByMountainId(mountainId);
+        return ApiResponse.success(GetBasesRes.from(mountainId, baseDTOS), HttpStatus.OK);
+    }
+}
