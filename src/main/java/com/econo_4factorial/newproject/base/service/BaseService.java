6869aff3207e23d4198329595889a83e7fb3c178
xxbeann <yjb4945@gmail.com>
2025-07-19 00:59:37 +0900

    feat: getBaseDetailsByMountainId 작성

--- src/main/java/com/econo_4factorial/newproject/base/service/BaseService.java
+++ src/main/java/com/econo_4factorial/newproject/base/service/BaseService.java
@@ -1,7 +1,11 @@
 package com.econo_4factorial.newproject.base.service;
 
+import com.econo_4factorial.newproject.base.domain.Base;
+import com.econo_4factorial.newproject.base.domain.BaseImage;
 import com.econo_4factorial.newproject.base.dto.BaseDTO;
+import com.econo_4factorial.newproject.base.dto.BaseDetailDTO;
 import com.econo_4factorial.newproject.base.exception.BadRequestException.BaseNotFoundException;
+import com.econo_4factorial.newproject.base.repository.BaseImageRepository;
 import com.econo_4factorial.newproject.base.repository.BaseRepository;
 import com.econo_4factorial.newproject.mountain.service.MountainService;
 import lombok.RequiredArgsConstructor;
@@ -14,6 +18,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class BaseService {
     private final BaseRepository baseRepository;
+    private final BaseImageRepository baseImageRepository;
     private final MountainService mountainService;
 
     @Transactional(readOnly = true)
@@ -23,6 +28,25 @@ public class BaseService {
         return baseRepository.findByMountainId(mountainId)
                 .stream()
                 .map(BaseDTO::from)
+                .toList();
+    }
+
+    @Transactional(readOnly = true)
+    public List<BaseDetailDTO> getBaseDetailsByMountainId(Long mountainId) {
+        mountainService.isMountainExistOrThrow(mountainId);
+
+        List<Base> bases = baseRepository.findByMountainId(mountainId);
+
+        System.out.println("가져온 Base 개수: " + bases.size());
+
+        return bases.stream()
+                .map(base -> {
+                    System.out.println("현재 처리 중인 Base ID: " + base.getId());
+                    System.out.println("현재 처리 중인 Base 이름: " + base.getName());
+                    List<BaseImage> baseImages = baseImageRepository.findByBaseId(base.getId());
+                    System.out.println("연결된 이미지 수: " + baseImages.size());
+                    return BaseDetailDTO.from(base, baseImages);
+                })
                 .toList();
     }
 }
