f167ba949433773add6ed8d089a2171c1949cfaf
xxbeann <yjb4945@gmail.com>
2025-07-19 20:10:46 +0900

    feat: 코스 상세 조회 Controller 작성

--- src/main/java/com/econo_4factorial/newproject/course/service/CourseService.java
+++ src/main/java/com/econo_4factorial/newproject/course/service/CourseService.java
@@ -1,9 +1,10 @@
 package com.econo_4factorial.newproject.course.service;
 
+import com.econo_4factorial.newproject.base.dto.CourseDetailDTO;
 import com.econo_4factorial.newproject.course.dto.CourseSearchCondition;
 import com.econo_4factorial.newproject.course.dto.res.CourseDTO;
 import com.econo_4factorial.newproject.course.repository.CourseCustomRepository;
-import com.econo_4factorial.newproject.user.service.UserService;
+import com.econo_4factorial.newproject.course.repository.CourseRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,11 +14,18 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class CourseService {
-    private final CourseCustomRepository courseCustomRepository;
+    private final CourseRepository courseRepository;
 
     @Transactional(readOnly = true)
     public List<CourseDTO> getAllCoursesWithBookmark(Long userId, Long mountainId, String sortBy) {
         CourseSearchCondition courseSearchCondition = CourseSearchCondition.of(mountainId, sortBy);
-        return courseCustomRepository.findAllByMountainIdWithBookmark(courseSearchCondition, mountainId, userId);
+        return courseRepository.findAllByMountainIdWithBookmark(courseSearchCondition, mountainId, userId);
+    }
+
+    @Transactional(readOnly = true)
+    public CourseDetailDTO getCourseDetailsByCourseId(Long courseId) {
+        return courseRepository.findById(courseId)
+                .map(CourseDetailDTO::from)
+                .orElseThrow(); // 머지 후 COURSENOTFOUNDEXCEPTION으로 리팩토링
     }
 }
