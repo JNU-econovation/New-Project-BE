228d11373fe288cfa4309986773e6e4180b138ca
Wooaje-jeong <woo6388@naver.com>
2025-07-20 01:02:22 +0900

    refactor: JPA가 지원하는 메소드 삭제

--- src/main/java/com/econo_4factorial/newproject/course/repository/CourseRepository.java
+++ src/main/java/com/econo_4factorial/newproject/course/repository/CourseRepository.java
@@ -6,5 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface CourseRepository extends JpaRepository<Course, Long>, CourseCustomRepository {
-    Optional<Course> findById(Long courseId);
 }
