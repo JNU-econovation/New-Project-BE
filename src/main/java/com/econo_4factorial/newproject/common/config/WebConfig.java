daf39b32a41d1794301ce870984450e1da4a482d
xxbeann <yjb4945@gmail.com>
2025-07-19 23:35:30 +0900

    feat: WebConfig에 JwtInterCeptor 추가

--- src/main/java/com/econo_4factorial/newproject/common/config/WebConfig.java
+++ src/main/java/com/econo_4factorial/newproject/common/config/WebConfig.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -13,6 +14,7 @@ import java.util.List;
 @AllArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
     private final UserIdResolver userIdResolver;
+    private final JwtInterceptor jwtInterceptor;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -36,6 +38,15 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(userIdResolver);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(jwtInterceptor)
+                .addPathPatterns("/api/**")
+                .excludePathPatterns("/api/v1/oauth/**");
+                    // 추후 스웨거 등 추가.
+                    // /swagger-ui/**, /swagger-resources/**, /v3/api-docs/
     }
 
 }
