fe1cd9dc15b7fe66ff340ad998b347d3b7c34f71
xxbeann <yjb4945@gmail.com>
2025-07-21 22:24:58 +0900

    feat: SecuritySceme 어노테이션 방식으로 추가

--- src/main/java/com/econo_4factorial/newproject/common/config/SwaggerConfig.java
+++ src/main/java/com/econo_4factorial/newproject/common/config/SwaggerConfig.java
@@ -1,7 +1,10 @@
 package com.econo_4factorial.newproject.common.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
 
 @OpenAPIDefinition(
@@ -9,7 +12,14 @@ import org.springframework.context.annotation.Configuration;
                 title = "San-Gyeol API",
                 version = "v1",
                 description = "산결 API 문서"
-        )
+        ),
+        security = @SecurityRequirement(name = "BearerAuth")
+)
+@SecurityScheme(
+        name = "BearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
 )
 @Configuration
 public class SwaggerConfig {
