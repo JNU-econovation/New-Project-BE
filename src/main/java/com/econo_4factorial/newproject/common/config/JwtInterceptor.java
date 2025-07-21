989a11c66354d3ff98907708575be5d0dd856263
xxbeann <yjb4945@gmail.com>
2025-07-21 22:22:58 +0900

    feat: JWT Interceptor 초기 구현

--- src/main/java/com/econo_4factorial/newproject/common/config/JwtInterceptor.java
+++ src/main/java/com/econo_4factorial/newproject/common/config/JwtInterceptor.java
@@ -11,13 +11,16 @@ public class JwtInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String uri = request.getRequestURI();
-        System.out.println("uri: " + uri);
-
         String token = request.getHeader("Authorization");
-        System.out.println("token: " + token);
+        System.out.println("[INTERCEPTOR] URI: " + uri);
+        System.out.println("[INTERCEPTOR] Authorization Header: " + token);
+
 
         if (token ==null || !token.startsWith("Bearer")) {
+
             response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+            System.out.println("[INTERCEPTOR] ❌ 토큰이 없거나 Bearer 형식이 아님 - 401 반환");
+            return false;
         }
 
         return true;
