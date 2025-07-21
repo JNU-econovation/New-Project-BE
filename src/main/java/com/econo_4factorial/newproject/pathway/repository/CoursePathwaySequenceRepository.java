a18467885363acfdf96b4682bbf9a4f2061c49b9
Wooaje-jeong <woo6388@naver.com>
2025-07-22 02:31:41 +0900

    feat: CoursePathwaySequenceRepository 구현

--- src/main/java/com/econo_4factorial/newproject/pathway/repository/CoursePathwaySequenceRepository.java
+++ src/main/java/com/econo_4factorial/newproject/pathway/repository/CoursePathwaySequenceRepository.java
@@ -4,6 +4,9 @@ import com.econo_4factorial.newproject.pathway.domain.CoursePathwaySequence;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CoursePathwaySequenceRepository extends JpaRepository<CoursePathwaySequence, Long> {
+    List<CoursePathwaySequence> findByCourseId(Long courseId);
 }
