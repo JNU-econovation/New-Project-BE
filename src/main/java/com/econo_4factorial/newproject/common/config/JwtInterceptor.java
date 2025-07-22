c6920433cca410e24b30af10eb7812e1438b5486
xxbeann <yjb4945@gmail.com>
2025-07-23 00:10:34 +0900

    feat: CORS preflight 요청은 토큰 검증을 하지 않습니다.

--- src/main/java/com/econo_4factorial/newproject/common/config/JwtInterceptor.java
+++ src/main/java/com/econo_4factorial/newproject/common/config/JwtInterceptor.java
@@ -3,6 +3,7 @@ package com.econo_4factorial.newproject.common.config;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
+import org.springframework.web.cors.CorsUtils;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 @Component
@@ -10,6 +11,11 @@ public class JwtInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        if(CorsUtils.isPreFlightRequest(request)) {
+            return true;
+        }
+
         String uri = request.getRequestURI();
         String token = request.getHeader("Authorization");
         System.out.println("[INTERCEPTOR] URI: " + uri);
