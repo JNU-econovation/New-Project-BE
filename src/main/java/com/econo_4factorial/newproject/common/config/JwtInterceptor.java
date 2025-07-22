f62a2e769debcad3c49d3c2998608d8ec69e7cee
xxbeann <yjb4945@gmail.com>
2025-07-23 00:34:47 +0900

    feat: 토큰 추출 후 refreshToken 유효성 검사
    토큰 추출 후 refreshToken을 유효성 검사합니다.
    이후 true 반환 시 인터셉터를 통과시킵니다.

--- src/main/java/com/econo_4factorial/newproject/common/config/JwtInterceptor.java
+++ src/main/java/com/econo_4factorial/newproject/common/config/JwtInterceptor.java
@@ -1,7 +1,9 @@
 package com.econo_4factorial.newproject.common.config;
 
+import com.econo_4factorial.newproject.auth.jwt.service.JwtTokenProvider;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 import org.springframework.web.cors.CorsUtils;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -9,26 +11,31 @@ import org.springframework.web.servlet.HandlerInterceptor;
 @Component
 public class JwtInterceptor implements HandlerInterceptor {
 
+    private static final String REISSUE_URI = "/api/v1/oauth/reissue";
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public JwtInterceptor(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
 
         if(CorsUtils.isPreFlightRequest(request)) {
             return true;
         }
 
-        String uri = request.getRequestURI();
-        String token = request.getHeader("Authorization");
-        System.out.println("[INTERCEPTOR] URI: " + uri);
-        System.out.println("[INTERCEPTOR] Authorization Header: " + token);
+        String token = jwtTokenProvider.extractToken(request.getHeader(HttpHeaders.AUTHORIZATION));
 
-
-        if (token ==null || !token.startsWith("Bearer")) {
-
-            response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
-            System.out.println("[INTERCEPTOR] ❌ 토큰이 없거나 Bearer 형식이 아님 - 401 반환");
-            return false;
+        if (request.getRequestURI().startsWith(REISSUE_URI)) {
+            return jwtTokenProvider.validateRefreshToken(token);
         }
 
-        return true;
+
+        String uri = request.getRequestURI();
+        System.out.println("[INTERCEPTOR] URI: " + uri);
+        System.out.println("[INTERCEPTOR] Authorization: " + token);
+
+        return false;
     }
 }
