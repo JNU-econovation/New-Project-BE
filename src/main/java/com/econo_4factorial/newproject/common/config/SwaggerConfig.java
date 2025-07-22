1d44b6e22c484fb2a852f72c0fe3557b81b0cc8d
xxbeann <yjb4945@gmail.com>
2025-07-22 22:39:22 +0900

    feat: swaggerInfo 어노테이션 방식에서 Bean 방식으로 변경

--- src/main/java/com/econo_4factorial/newproject/common/config/SwaggerConfig.java
+++ src/main/java/com/econo_4factorial/newproject/common/config/SwaggerConfig.java
@@ -1,26 +1,37 @@
 package com.econo_4factorial.newproject.common.config;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.*;
+import jakarta.servlet.ServletContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@OpenAPIDefinition(
-        info = @Info(
-                title = "San-Gyeol API",
-                version = "v1",
-                description = "산결 API 문서"
-        ),
-        security = @SecurityRequirement(name = "BearerAuth")
-)
-@SecurityScheme(
-        name = "BearerAuth",
-        type = SecuritySchemeType.HTTP,
-        scheme = "bearer",
-        bearerFormat = "JWT"
-)
+//@SecurityScheme(
+//        name = "BearerAuth",
+//        type = SecuritySchemeType.HTTP,
+//        scheme = "bearer",
+//        bearerFormat = "JWT"
+//)
 @Configuration
 public class SwaggerConfig {
+
+        @Bean
+        public OpenAPI OpenAPI(ServletContext servletContext) {
+
+                return new OpenAPI()
+                        .info(swaggerInfo());
+
+        }
+
+        private  Info swaggerInfo() {
+                License license = new License();
+                license.setUrl("https://github.com/JNU-econovation/New-Project-BE.git");
+                license.setName("산결");
+
+                return new Info()
+                        .version("v1.0.0")
+                        .title("San-Gyeol API")
+                        .description("산결 API 문서입니다.")
+                        .license(license);
+        }
 }
