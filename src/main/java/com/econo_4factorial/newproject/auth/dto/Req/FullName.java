cf95da89cae402fffeff66c5ee79810c5723dfda
Wooaje-jeong <woo6388@naver.com>
2025-07-23 23:04:24 +0900

    feat: FUllName DTO 내부에서 성과 이름을 결합한 성함을 반환하는 메소드 구현

--- src/main/java/com/econo_4factorial/newproject/auth/dto/Req/FullName.java
+++ src/main/java/com/econo_4factorial/newproject/auth/dto/Req/FullName.java
@@ -5,4 +5,10 @@ public record FullName(
 
         String givenName
 ) {
+    public String getName() {
+        if (this.familyName == null || this.givenName == null ||
+                this.familyName.isBlank() || this.givenName.isBlank())
+            return null;
+        return this.familyName + this.givenName;
+    }
 }
