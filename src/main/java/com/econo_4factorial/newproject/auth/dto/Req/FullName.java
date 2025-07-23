3ddba8b7caaaa1000f04bbcbb3ca71de5def50ce
Wooaje-jeong <woo6388@naver.com>
2025-07-23 22:43:18 +0900

    refactor: AppleLoginReq 유효성 검사 수정
    애플로그인 시, 애플에서 identityToken을 제외한 나머지 값들에 null값이 올 수 있어서 이에 대한 유효성 검사 삭제

--- src/main/java/com/econo_4factorial/newproject/auth/dto/Req/FullName.java
+++ src/main/java/com/econo_4factorial/newproject/auth/dto/Req/FullName.java
@@ -1,13 +1,8 @@
 package com.econo_4factorial.newproject.auth.dto.Req;
 
-import com.econo_4factorial.newproject.common.exception.ValidationMessage;
-import jakarta.validation.constraints.NotBlank;
-
 public record FullName(
-        @NotBlank(message = ValidationMessage.FAMILY_NAME_REQUIRED)
         String familyName,
 
-        @NotBlank(message = ValidationMessage.GIVEN_NAME_REQUIRED)
         String givenName
 ) {
 }
