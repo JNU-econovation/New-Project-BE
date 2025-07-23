05b1b738eb10f3e62b7eff2fb2aacbdc0fe57507
Wooaje-jeong <woo6388@naver.com>
2025-07-23 18:50:39 +0900

    refactor: fetchJoin을 통한 N+1 문제 해결

--- src/main/java/com/econo_4factorial/newproject/pathway/repository/CoursePathwaySequenceRepository.java
+++ src/main/java/com/econo_4factorial/newproject/pathway/repository/CoursePathwaySequenceRepository.java
@@ -2,11 +2,7 @@ package com.econo_4factorial.newproject.pathway.repository;
 
 import com.econo_4factorial.newproject.pathway.domain.CoursePathwaySequence;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-import java.util.List;
+public interface CoursePathwaySequenceRepository extends JpaRepository<CoursePathwaySequence, Long>, CoursePathwaySequenceCustomRepository {
 
-@Repository
-public interface CoursePathwaySequenceRepository extends JpaRepository<CoursePathwaySequence, Long> {
-    List<CoursePathwaySequence> findByCourseIdOrderBySequence(Long courseId);
 }
