03abe66af557e5dac171f6bdc8a6e170da483786
xxbeann <yjb4945@gmail.com>
2025-08-06 14:59:25 +0900

    refactor: BookmarkController Swaggger Description 추가

--- src/main/java/com/econo_4factorial/newproject/course/controller/BookmarkController.java
+++ src/main/java/com/econo_4factorial/newproject/course/controller/BookmarkController.java
@@ -7,6 +7,9 @@ import com.econo_4factorial.newproject.course.dto.CourseWithBookmarkDTO;
 import com.econo_4factorial.newproject.course.dto.req.AddBookmarkReq;
 import com.econo_4factorial.newproject.course.dto.res.GetBookmarkListRes;
 import com.econo_4factorial.newproject.course.service.BookmarkService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -16,13 +19,17 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/bookmarks")
+@Tag(name = "Bookmark", description = "즐겨찾기 관련 API")
 @AllArgsConstructor
 public class BookmarkController {
     private final BookmarkService bookmarkService;
 
     @PostMapping
+    @Operation(summary = "즐겨찾기 추가", description = "사용자의 즐겨찾기를 추가합니다.")
     public ApiResult<ApiResult.SuccessBody<Void>> addBookmark (
+            @Parameter(name = "userId", description = "사용자 ID", required = true)
             @UserId Long userId,
+
             @RequestBody @Valid AddBookmarkReq addBookmarkReq
     ) {
         bookmarkService.addBookmark(userId, addBookmarkReq.courseId());
@@ -30,16 +37,22 @@ public class BookmarkController {
     }
 
     @DeleteMapping({"/{courseId}"})
+    @Operation(summary = "즐겨찾기 삭제", description = "사용자의 즐겨찾기를 삭제합니다.")
     public ApiResult<ApiResult.SuccessBody<Void>> deleteBookmark (
+            @Parameter(name = "userId", description = "사용자 ID", required = true)
             @UserId Long userId,
-            @PathVariable(name="courseId") Long courseId
+
+            @Parameter(name = "courseId", description = "코스 ID", required = true)
+            @PathVariable Long courseId
     ) {
         bookmarkService.deleteBookmark(userId, courseId);
         return ApiResponse.success(HttpStatus.OK);
     }
 
     @GetMapping
+    @Operation(summary = "전체 즐겨찾기 조회", description = "사용자의 모든 즐겨찾기 리스트를 조회합니다.")
     public ApiResult<ApiResult.SuccessBody<GetBookmarkListRes>> getBookmarkList (
+            @Parameter(name = "userId", description = "사용자 ID", required = true)
             @UserId Long userId
     ) {
         List<CourseWithBookmarkDTO> bookmarks = bookmarkService.getBookmarkList(userId);
