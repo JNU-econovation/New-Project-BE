65b88b44abe9d0fa4d6ec3e36ec408394037b170
xxbeann <yjb4945@gmail.com>
2025-08-06 16:24:37 +0900

    refactor: Adding Swagger Description to PathwayController

--- src/main/java/com/econo_4factorial/newproject/pathway/controller/PathwayController.java
+++ src/main/java/com/econo_4factorial/newproject/pathway/controller/PathwayController.java
@@ -5,6 +5,9 @@ import com.econo_4factorial.newproject.common.util.api.ApiResult;
 import com.econo_4factorial.newproject.pathway.dto.PathwayDTO;
 import com.econo_4factorial.newproject.pathway.dto.res.GetPathwaysOfCourseRes;
 import com.econo_4factorial.newproject.pathway.service.PathwayService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,11 +20,14 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/v1/pathways")
+@Tag(name = "Pathway", description = "경로 관련 API")
 public class PathwayController {
     private final PathwayService pathwayService;
 
     @GetMapping
+    @Operation(summary = "경로 불러오기", description = "해당 코스의 경로 데이터를 불러옵니다.")
     public ApiResult<ApiResult.SuccessBody<GetPathwaysOfCourseRes>> getPathwaysOfCourse(
+            @Parameter(name = "courseId", description = "코스 ID", required = true)
             @RequestParam(name = "courseId") Long courseId
     ) {
         List<PathwayDTO> pathways = pathwayService.getPathwaysByCourseId(courseId);
