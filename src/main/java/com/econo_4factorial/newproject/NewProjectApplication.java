5eb2a3693452b5c47686ff94b09c512320c6d667
xxbeann <yjb4945@gmail.com>
2025-08-08 00:48:31 +0900

    chore: scheduler 어노테이션 추가

--- src/main/java/com/econo_4factorial/newproject/NewProjectApplication.java
+++ src/main/java/com/econo_4factorial/newproject/NewProjectApplication.java
@@ -6,10 +6,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableJpaAuditing
 @SpringBootApplication
 @EnableFeignClients
+@EnableScheduling
 @OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")}) // 추가
 public class NewProjectApplication {
 
