8549763fc055dba257e253ec6ac96f5a3aabc1c5
xxbeann <yjb4945@gmail.com>
2025-08-08 18:34:44 +0900

    hotfix: cors 허용 , 추가

--- src/main/java/com/econo_4factorial/newproject/common/config/WebConfig.java
+++ src/main/java/com/econo_4factorial/newproject/common/config/WebConfig.java
@@ -28,7 +28,7 @@ public class WebConfig implements WebMvcConfigurer {
                         "http://10.20.10.176:3000", // JNU
                         "http://10.20.10.217:3000", // JNU
                         "http://192.168.0.240:3000", // JNU
-                        "https://soop.euichan.com/" // 프론트 배포 주소
+                        "https://soop.euichan.com/", // 프론트 배포 주소
                         "econo.soop.euichan.com:17779"// 동방
                         )
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
