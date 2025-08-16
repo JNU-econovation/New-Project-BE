0f3c85b2eae7d54f7e3adce4d028d551990eddc1
xxbeann <yjb4945@gmail.com>
2025-08-16 23:33:48 +0900

    feat: QueryDSL 사용해서 peak_base_id 직접 프로젝션

--- src/main/java/com/econo_4factorial/newproject/course/repository/CourseCustomRepositoryImpl.java
+++ src/main/java/com/econo_4factorial/newproject/course/repository/CourseCustomRepositoryImpl.java
@@ -10,7 +10,6 @@ import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.core.types.dsl.CaseBuilder;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.AllArgsConstructor;
-import org.aspectj.weaver.ast.Or;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -39,7 +38,7 @@ public class CourseCustomRepositoryImpl implements CourseCustomRepository {
                         course.id,
                         course.name,
                         course.displayName,
-                        course.peakBaseId,
+                        course.peakBase.id,
                         course.length,
                         course.duration,
                         course.difficulty,
