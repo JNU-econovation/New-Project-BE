64def851106bee57a3238e0b7ea5c5317861464f
Woojae-Jeong <woo6388@naver.com>
2025-08-25 22:29:12 +0900

    feat: 유저의 프로필 설정 여부 확인 API 구현

---
+++ src/main/java/com/econo_4factorial/newproject/user/UserController.java
@@ -0,0 +1,28 @@
+package com.econo_4factorial.newproject.user;
+
+import com.econo_4factorial.newproject.common.annotation.UserId;
+import com.econo_4factorial.newproject.common.util.api.ApiResponse;
+import com.econo_4factorial.newproject.common.util.api.ApiResult;
+import com.econo_4factorial.newproject.user.dto.res.GetProfileStatusRes;
+import com.econo_4factorial.newproject.user.service.UserService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("api/v1/users")
+@Tag(name = "User", description = "유저 관련 API")
+public class UserController {
+    private final UserService userService;
+
+    @GetMapping("/profile/status")
+    public ApiResult<ApiResult.SuccessBody<GetProfileStatusRes>> getProfileStatus (@UserId Long userId) {
+        Boolean result = userService.isProfileComplete(userId);
+        return ApiResponse.success(GetProfileStatusRes.from(result), HttpStatus.OK);
+    }
+
+}
