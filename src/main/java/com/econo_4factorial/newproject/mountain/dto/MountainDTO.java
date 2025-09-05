21c4e501c72dea3edb1144d714c24f3bdb29d5ec
xxbeann <yjb4945@gmail.com>
2025-09-05 22:13:43 +0900

    feat: 반환 값에 coordinate 추가
    MountainDTO에 coordinate를 List 형태로 추가했었습니다.
    다만, 이전 data.sql에 위도 경도 값을 NULL값으로 두었었는데 이것이 MountainDTO list.of 메서드에서 NPE을 일으켰습니다.
    해결방법을 고민하던 중 어차피 산을 대표하는 위도 경도 값은 NULL값이 아닐 것이며, 초반에 스키마를 NOT NULL값으로 구성하였습니다.
    그래서 스키마를 nullable=false로 수정 후, 목 데이터 값을 집어넣었습니다.

--- src/main/java/com/econo_4factorial/newproject/mountain/dto/MountainDTO.java
+++ src/main/java/com/econo_4factorial/newproject/mountain/dto/MountainDTO.java
@@ -2,16 +2,21 @@ package com.econo_4factorial.newproject.mountain.dto;
 
 import com.econo_4factorial.newproject.mountain.domain.Mountain;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 public record MountainDTO(
         Long id,
         String name,
-        String location
+        String location,
+        List<BigDecimal> coordinate
 ) {
     public static MountainDTO from(Mountain mountain) {
         return new MountainDTO(
                 mountain.getId(),
                 mountain.getName(),
-                mountain.getLocation()
+                mountain.getLocation(),
+                List.of(mountain.getLongitude(), mountain.getLatitude())
         );
     }
 }
