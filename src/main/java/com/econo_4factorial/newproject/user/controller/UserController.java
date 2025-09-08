fe6abc0d9bba183aac043aa189b2be23cbbe3550
Woojae-Jeong <woo6388@naver.com>
2025-09-09 02:38:49 +0900

    feat: 랜덤 닉네임 API 구현

--- src/main/java/com/econo_4factorial/newproject/user/controller/UserController.java
+++ src/main/java/com/econo_4factorial/newproject/user/controller/UserController.java
@@ -4,6 +4,8 @@ import com.econo_4factorial.newproject.common.annotation.UserId;
 import com.econo_4factorial.newproject.common.util.api.ApiResponse;
 import com.econo_4factorial.newproject.common.util.api.ApiResult;
 import com.econo_4factorial.newproject.user.dto.res.GetProfileStatusRes;
+import com.econo_4factorial.newproject.user.dto.res.GetRandomNicknameRes;
+import com.econo_4factorial.newproject.user.service.RandomNicknameService;
 import com.econo_4factorial.newproject.user.service.UserService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
@@ -18,11 +20,18 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "User", description = "유저 관련 API")
 public class UserController {
     private final UserService userService;
+    private final RandomNicknameService randomNicknameService;
 
     @GetMapping("/profile/status")
     public ApiResult<ApiResult.SuccessBody<GetProfileStatusRes>> getProfileStatus (@UserId Long userId) {
         Boolean result = userService.isProfileFilled(userId);
         return ApiResponse.success(GetProfileStatusRes.from(result), HttpStatus.OK);
+    }
+
+    @GetMapping("/nickname/random")
+    public ApiResult<ApiResult.SuccessBody<GetRandomNicknameRes>> getRandomNickname () {
+        String nickname = randomNicknameService.getRandomNickname();
+        return ApiResponse.success(GetRandomNicknameRes.from(nickname), HttpStatus.OK);
     }
 
 }
