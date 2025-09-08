24b50ab0cfeb49ff2d6548231ca8ae5b5ac68253
xxbeann <yjb4945@gmail.com>
2025-09-08 23:56:12 +0900

    feat: findByInitials 추가 및 메서드 명 변경

--- src/main/java/com/econo_4factorial/newproject/mountain/repository/MountainRepository.java
+++ src/main/java/com/econo_4factorial/newproject/mountain/repository/MountainRepository.java
@@ -9,6 +9,6 @@ import java.util.List;
 @Repository
 public interface MountainRepository extends JpaRepository<Mountain, Long> {
     List<Mountain> findAllByOrderByNameAsc();
-
-    List<Mountain> findByNameContaining(String keyword);
+    List<Mountain> findByInitials(String keyword);
+    List<Mountain> findByMountainName(String keyword);
 }
