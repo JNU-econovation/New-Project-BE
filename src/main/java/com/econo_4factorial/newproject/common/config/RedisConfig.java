781839d06d1d912956c48ec1f3a547dbc90ed3e1
xxbeann <yjb4945@gmail.com>
2025-09-18 23:41:24 +0900

    feat: StringRedisTemplate 추가

--- src/main/java/com/econo_4factorial/newproject/common/config/RedisConfig.java
+++ src/main/java/com/econo_4factorial/newproject/common/config/RedisConfig.java
@@ -7,6 +7,7 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 import java.time.Duration;
 
@@ -29,5 +30,12 @@ public class RedisConfig {
                 //.useSsl()
                 .build();
         return new LettuceConnectionFactory(redisConfiguration, clientConfig);
+    }
+
+    @Bean
+    public StringRedisTemplate stringRedisTemplate() {
+        StringRedisTemplate template = new StringRedisTemplate();
+        template.setConnectionFactory(redisConnectionFactory());
+        return template;
     }
 }
