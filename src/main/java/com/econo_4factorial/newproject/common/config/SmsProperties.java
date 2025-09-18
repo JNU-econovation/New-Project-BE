7400216c55510e02949404e177d966403bacc4f8
xxbeann <yjb4945@gmail.com>
2025-09-18 22:34:13 +0900

    feat: properties config 추가

---
+++ src/main/java/com/econo_4factorial/newproject/common/config/SmsProperties.java
@@ -0,0 +1,16 @@
+package com.econo_4factorial.newproject.common.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+@Component
+@ConfigurationProperties(prefix = "coolsms.api")
+public class SmsProperties {
+    private String key;
+    private String secret;
+    private String number;
+}
