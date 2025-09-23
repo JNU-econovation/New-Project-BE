c22ae011dd134ca8ac0f3f0783ada99fe531b738
xxbeann <yjb4945@gmail.com>
2025-09-23 21:01:19 +0900

    feat: SMS 인증 기능 구현

--- src/main/java/com/econo_4factorial/newproject/auth/service/SmsService.java
+++ src/main/java/com/econo_4factorial/newproject/auth/service/SmsService.java
@@ -1,5 +1,7 @@
 package com.econo_4factorial.newproject.auth.service;
 
+import com.econo_4factorial.newproject.auth.exception.InternalServerException.IncorrectVerificationCodeException;
+import com.econo_4factorial.newproject.auth.exception.InternalServerException.InvalidVerificationCodeException;
 import com.econo_4factorial.newproject.auth.repository.SmsRepository;
 import com.econo_4factorial.newproject.user.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -21,5 +23,17 @@ public class SmsService {
         String verificationCode = smsUtilService.generateVerificationCode();
         smsRepository.createSmsVerification(normalizedPhoneNumber, verificationCode);
         smsUtilService.sendMessage(normalizedPhoneNumber, verificationCode);
+    }
+
+    public void verifySms(String toPhoneNumber, String verificationCode) {
+        String normalizedPhoneNumber = smsUtilService.normalizePhoneNumber(toPhoneNumber);
+        String storedCode = smsRepository.getSmsVerification(normalizedPhoneNumber);
+        if (storedCode == null) {
+            throw new InvalidVerificationCodeException();
+        }
+        if (!storedCode.equals(verificationCode)) {
+            throw new IncorrectVerificationCodeException();
+        }
+        smsRepository.deleteSmsVerification(normalizedPhoneNumber);
     }
 }
