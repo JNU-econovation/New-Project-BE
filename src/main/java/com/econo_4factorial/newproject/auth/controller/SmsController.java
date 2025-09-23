9074d2820da66f349facf6356a960d6ca4745542
xxbeann <yjb4945@gmail.com>
2025-09-23 16:44:19 +0900

    feat: verify controller 생성

--- src/main/java/com/econo_4factorial/newproject/auth/controller/SmsController.java
+++ src/main/java/com/econo_4factorial/newproject/auth/controller/SmsController.java
@@ -1,7 +1,9 @@
 package com.econo_4factorial.newproject.auth.controller;
 
 import com.econo_4factorial.newproject.auth.dto.Req.SendSmsReq;
+import com.econo_4factorial.newproject.auth.dto.Req.VerifySmsReq;
 import com.econo_4factorial.newproject.auth.dto.Res.SendSmsRes;
+import com.econo_4factorial.newproject.auth.dto.Res.VerifySmsRes;
 import com.econo_4factorial.newproject.auth.service.SmsService;
 import com.econo_4factorial.newproject.common.util.api.ApiResponse;
 import com.econo_4factorial.newproject.common.util.api.ApiResult;
@@ -29,8 +31,17 @@ public class SmsController {
     @Operation(summary = "인증번호 전송", description = "인증번호를 전송합니다.")
     public ApiResult<ApiResult.SuccessBody<SendSmsRes>> sendSms(
             @RequestBody @Valid SendSmsReq sendSmsReq
-            ) {
+    ) {
         smsService.sendSms(sendSmsReq.phoneNumber());
         return ApiResponse.success(SendSmsRes.from(sendSmsReq.phoneNumber()),HttpStatus.OK);
+    }
+
+    @PostMapping("/verify")
+    @Operation(summary = "인증번호 확인", description = "인증번호를 확인합니다.")
+    public ApiResult<ApiResult.SuccessBody<VerifySmsRes>> verifySms(
+            @RequestBody @Valid VerifySmsReq verifySmsReq
+    ) {
+        smsService.verifySms(verifySmsReq.phoneNumber(),verifySmsReq.verificationCode());
+        return ApiResponse.success(VerifySmsRes.from(verifySmsReq.phoneNumber()),HttpStatus.OK);
     }
 }
