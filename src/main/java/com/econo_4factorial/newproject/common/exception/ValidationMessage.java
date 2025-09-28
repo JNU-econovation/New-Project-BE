ae258e5ad22b99cbf91f57e84d19ad166083b16e
xxbeann <yjb4945@gmail.com>
2025-09-28 16:00:02 +0900

    refactor: 오류 메시지 수정

--- src/main/java/com/econo_4factorial/newproject/common/exception/ValidationMessage.java
+++ src/main/java/com/econo_4factorial/newproject/common/exception/ValidationMessage.java
@@ -10,6 +10,6 @@ public class ValidationMessage {
     public static final String POSITIVE_NUMBER_REQUIRED = "body의 특정 필드가 양수여야 합니다";
     public static final String PHONE_NUMBER_IS_REQUIRED = "전화번호는 필수입니다.";
     public static final String PHONE_NUMBER_INVALID = "전화번호 형식이 올바르지 않습니다.";
-    public static final String VERIFICATION_CODE_IS_REQUIRED = "전화번호 형식이 올바르지 않습니다.";
-    public static final String VERIFICATION_CODE_INVALID = "전화번호 형식이 올바르지 않습니다.";
+    public static final String VERIFICATION_CODE_IS_REQUIRED = "인증번호는 필수입니다.";
+    public static final String VERIFICATION_CODE_INVALID = "인증번호 형식이 올바르지 않습니다.";
 }
