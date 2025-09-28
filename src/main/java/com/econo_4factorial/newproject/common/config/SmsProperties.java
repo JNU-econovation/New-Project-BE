3425bef3db714780a68d76bd6a83dfb13cc45038
xxbeann <yjb4945@gmail.com>
2025-09-28 16:25:42 +0900

    refactor: 유효성 검사 추가
    SmsProperties에 유효성 검사를 추가함으로써, 잘못된 설정을 애플리케이션 부팅 시점에 조기 감지합니다.

--- src/main/java/com/econo_4factorial/newproject/common/config/SmsProperties.java
+++ src/main/java/com/econo_4factorial/newproject/common/config/SmsProperties.java
@@ -1,18 +1,26 @@
 package com.econo_4factorial.newproject.common.config;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
 
 @Getter
 @Setter
 @Component
 @ConfigurationProperties(prefix = "coolsms.api")
+@Validated
 public class SmsProperties {
+    @NotBlank
     private String key;
+    @NotBlank
     private String secret;
+    @NotBlank
     private String senderNumber;
     private String smsPrefix;
+    @Positive
     private int ttl;
 }
