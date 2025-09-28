e4ccb6d51734fbef5f2482a9039f97f742c28b9f
xxbeann <yjb4945@gmail.com>
2025-09-28 16:16:23 +0900

    refactor: ErrorType 재설정
    기존에 있던 500 InternealServerException을 400계열의 BadRequset로 변경했습니다.

--- src/main/java/com/econo_4factorial/newproject/auth/exception/InternalServerException/InvalidVerificationCodeException.java
+++ src/main/java/com/econo_4factorial/newproject/auth/exception/InternalServerException/InvalidVerificationCodeException.java
@@ -1,9 +1,9 @@
 package com.econo_4factorial.newproject.auth.exception.InternalServerException;
 
 import com.econo_4factorial.newproject.auth.exception.AuthErrorType;
-import com.econo_4factorial.newproject.common.exception.InternalServerException;
+import com.econo_4factorial.newproject.common.exception.BadRequestException;
 
-public class InvalidVerificationCodeException extends InternalServerException {
+public class InvalidVerificationCodeException extends BadRequestException {
     public InvalidVerificationCodeException() {
         super(AuthErrorType.INVALID_VERIFICATION_CODE_EXCEPTION);
     }
