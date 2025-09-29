4c0d00343176c4c4297b196f22b9f48f45f23c00
xxbeann <yjb4945@gmail.com>
2025-09-30 01:39:07 +0900

    feat: ValidatoinMessage 추가 및 명명 정규화

--- src/main/java/com/econo_4factorial/newproject/common/exception/ValidationMessage.java
+++ src/main/java/com/econo_4factorial/newproject/common/exception/ValidationMessage.java
@@ -4,10 +4,13 @@ public class ValidationMessage {
     public static final String IDENTITY_TOKEN_REQUIRED = "identityToken이 누락되었습니다";
     public static final String FAMILY_NAME_REQUIRED = "familyName이 누락되었습니다";
     public static final String GIVEN_NAME_REQUIRED = "givenName이 누락되었습니다";
-    public static final String EMAIL_INVALID_PATTERN = "email이 올바르지 않은 패턴입니다";
+    public static final String EMAIL_PATTERN_INVALID = "email이 올바르지 않은 패턴입니다";
     public static final String REFRESH_TOKEN_REQUIRED = "refreshToken이 누락되었습니다";
     public static final String COURSE_ID_REQUIRED = "courseId가 누락되었습니다";
     public static final String POSITIVE_NUMBER_REQUIRED = "body의 특정 필드가 양수여야 합니다";
+    public static final String NICKNAME_IS_REQUIRED = "nickname이 누락되었습니다.";
+    public static final String NICKNAME_LENGTH_INVALID = "닉네임은 2자 이상 12자 이하로 입력해야 합니다.";
+    public static final String NICKNAME_PATTERN_INVALID = "닉네임은 한글, 영문, 숫자만 입력할 수 있습니다.";;
     public static final String PHONE_NUMBER_IS_REQUIRED = "전화번호는 필수입니다.";
     public static final String PHONE_NUMBER_INVALID = "전화번호 형식이 올바르지 않습니다.";
     public static final String VERIFICATION_CODE_IS_REQUIRED = "인증번호는 필수입니다.";
