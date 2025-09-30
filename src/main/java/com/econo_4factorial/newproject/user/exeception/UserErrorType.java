56eda03fa51e992d735fc5bf6bd57a8400426eb1
xxbeann <yjb4945@gmail.com>
2025-09-30 20:50:31 +0900

    feat: registerBasicInformation 서비스 작성

--- src/main/java/com/econo_4factorial/newproject/user/exeception/UserErrorType.java
+++ src/main/java/com/econo_4factorial/newproject/user/exeception/UserErrorType.java
@@ -5,7 +5,8 @@ import org.springframework.http.HttpStatus;
 
 public enum UserErrorType implements ErrorType {
     USER_NOT_FOUND_EXCEPTION ("USER400_001", HttpStatus.NOT_FOUND, "회원을 찾을 수 없습니다"),
-    PHONE_NUMBER_ALREADY_EXISTS_EXCEPTION("USER400_002", HttpStatus.BAD_REQUEST, "이미 존재하는 전화번호입니다.");
+    PHONE_NUMBER_ALREADY_EXISTS_EXCEPTION("USER400_002", HttpStatus.BAD_REQUEST, "이미 존재하는 전화번호입니다."),
+    EMAIL_ALREADY_EXISTS_EXCEPTION("USER400_003", HttpStatus.BAD_REQUEST, "이미 존재하는 이메일입니다.");
 
     private final String errorCode;
     private final HttpStatus httpStatus;
