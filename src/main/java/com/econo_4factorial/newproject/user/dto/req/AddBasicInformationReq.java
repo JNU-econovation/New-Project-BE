7f2c64726ef6a38c6a3277174b8eaf8880daeaae
xxbeann <yjb4945@gmail.com>
2025-09-30 20:49:21 +0900

    feat: controller 및 reqDTO 작성

---
+++ src/main/java/com/econo_4factorial/newproject/user/dto/req/AddBasicInformationReq.java
@@ -0,0 +1,27 @@
+package com.econo_4factorial.newproject.user.dto.req;
+
+import com.econo_4factorial.newproject.common.annotation.ValidEmailPattern;
+import com.econo_4factorial.newproject.common.exception.ValidationMessage;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public record AddBasicInformationReq(
+        @NotNull(message = ValidationMessage.NICKNAME_IS_REQUIRED)
+        @Size(min = 2, max = 12, message = ValidationMessage.NICKNAME_LENGTH_INVALID)
+        @Pattern(
+                regexp = "^[가-힣a-zA-Z0-9]+$",
+                message = ValidationMessage.NICKNAME_PATTERN_INVALID
+        )
+        String nickname,
+        @NotBlank(message = ValidationMessage.PHONE_NUMBER_IS_REQUIRED)
+        @Pattern(
+                regexp = "^010-\\d{4}-\\d{4}$",
+                message = ValidationMessage.PHONE_NUMBER_INVALID
+        )
+        String phoneNumber,
+        @ValidEmailPattern
+        String email
+) {
+}
