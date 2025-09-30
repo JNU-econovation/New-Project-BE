21b57bd97a7958099a04c8ba67cebcf81200f43c
xxbeann <yjb4945@gmail.com>
2025-09-30 21:31:04 +0900

    feat: notNull 어노테이션 notBalnk로 변경

--- src/main/java/com/econo_4factorial/newproject/user/dto/req/CheckNicknameReq.java
+++ src/main/java/com/econo_4factorial/newproject/user/dto/req/CheckNicknameReq.java
@@ -1,12 +1,12 @@
 package com.econo_4factorial.newproject.user.dto.req;
 
 import com.econo_4factorial.newproject.common.exception.ValidationMessage;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record CheckNicknameReq(
-        @NotNull(message = ValidationMessage.NICKNAME_IS_REQUIRED)
+        @NotBlank(message = ValidationMessage.NICKNAME_IS_REQUIRED)
         @Size(min = 2, max = 12, message = ValidationMessage.NICKNAME_LENGTH_INVALID)
         @Pattern(
                 regexp = "^[가-힣a-zA-Z0-9]+$",
