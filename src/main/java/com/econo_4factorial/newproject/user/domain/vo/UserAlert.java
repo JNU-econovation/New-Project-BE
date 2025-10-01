3b43483cdfeddf705686b9689862555e79be7019
xxbeann <yjb4945@gmail.com>
2025-10-01 21:23:50 +0900

    feat: UserAlert VO 추가

---
+++ src/main/java/com/econo_4factorial/newproject/user/domain/vo/UserAlert.java
@@ -0,0 +1,29 @@
+package com.econo_4factorial.newproject.user.domain.vo;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class UserAlert {
+
+    @Column(name = "event_alert", nullable = false)
+    private boolean eventAlert = true;
+
+    @Column(name = "travel_deviation_alert", nullable = false)
+    private boolean travelDeviationAlert = true;
+
+    @Column(name = "accident_prone_area_alert", nullable = false)
+    private boolean accidentProneAreaAlert = true;
+
+    public void updateAlerts(boolean eventAlert, boolean travelDeviationAlert, boolean accidentProneAreaAlert) {
+        this.eventAlert = eventAlert;
+        this.travelDeviationAlert = travelDeviationAlert;
+        this.accidentProneAreaAlert = accidentProneAreaAlert;
+    }
+
+}
