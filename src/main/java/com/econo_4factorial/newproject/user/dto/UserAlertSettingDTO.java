ad461d0b4e3dc3dd001d56d0c0d59f923fb8cfa4
xxbeann <yjb4945@gmail.com>
2025-10-02 01:28:32 +0900

    refactor: DTO가 필요한 것만 의존하도록 축소

--- src/main/java/com/econo_4factorial/newproject/user/dto/UserAlertSettingDTO.java
+++ src/main/java/com/econo_4factorial/newproject/user/dto/UserAlertSettingDTO.java
@@ -1,17 +1,18 @@
 package com.econo_4factorial.newproject.user.dto;
 
 import com.econo_4factorial.newproject.user.domain.User;
+import com.econo_4factorial.newproject.user.domain.vo.UserAlert;
 
 public record UserAlertSettingDTO(
         boolean eventAlert,
         boolean travelDeviationAlert,
         boolean accidentProneAreaAlert
 ) {
-    public static UserAlertSettingDTO from(User user) {
+    public static UserAlertSettingDTO from(UserAlert userAlert) {
         return new UserAlertSettingDTO(
-                user.getUserAlert().isEventAlert(),
-                user.getUserAlert().isTravelDeviationAlert(),
-                user.getUserAlert().isAccidentProneAreaAlert()
+                userAlert.isEventAlert(),
+                userAlert.isTravelDeviationAlert(),
+                userAlert.isAccidentProneAreaAlert()
         );
     }
 }
