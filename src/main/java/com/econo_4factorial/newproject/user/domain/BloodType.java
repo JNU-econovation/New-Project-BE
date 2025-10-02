65948636c63b23caaaf6e01d9847ca1e666c98c7
xxbeann <yjb4945@gmail.com>
2025-10-02 18:24:18 +0900

    feat: String BloodType을 BloodType으로 변경

--- src/main/java/com/econo_4factorial/newproject/user/domain/BloodType.java
+++ src/main/java/com/econo_4factorial/newproject/user/domain/BloodType.java
@@ -1,8 +1,20 @@
 package com.econo_4factorial.newproject.user.domain;
 
+import com.econo_4factorial.newproject.common.exception.NullRequestException;
+import com.econo_4factorial.newproject.user.exeception.BadRequestException.InvalidBloodTypeException;
+
 public enum BloodType {
     A,
     B,
     O,
-    AB
+    AB;
+
+    public static BloodType fromString(String bloodType) {
+        if (bloodType == null) throw new NullRequestException();
+        try {
+            return BloodType.valueOf(bloodType.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            throw new InvalidBloodTypeException();
+        }
+    }
 }
