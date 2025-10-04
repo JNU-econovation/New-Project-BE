562ef5b80a71b8267deb736c75225f05909edba5
xxbeann <yjb4945@gmail.com>
2025-10-04 16:49:11 +0900

    feat: 프로필 설정여부 확인에 개인정보 반환 값 추가
    기존에 있던 response에 UserStatusInfoDTO를 만들어 값을 관리.

---
+++ src/main/java/com/econo_4factorial/newproject/user/dto/UserStatusInfoDTO.java
@@ -0,0 +1,7 @@
+package com.econo_4factorial.newproject.user.dto;
+
+public record UserStatusInfoDTO(
+        boolean isBasicInfoSet,
+        boolean isPersonalInfoSet
+) {
+}
--- src/main/java/com/econo_4factorial/newproject/user/dto/res/GetProfileStatusRes.java
+++ src/main/java/com/econo_4factorial/newproject/user/dto/res/GetProfileStatusRes.java
@@ -1,9 +1,12 @@
 package com.econo_4factorial.newproject.user.dto.res;
 
+import com.econo_4factorial.newproject.user.dto.UserInfoDTO;
+import com.econo_4factorial.newproject.user.dto.UserStatusInfoDTO;
+
 public record GetProfileStatusRes(
-        Boolean isComplete
+        UserStatusInfoDTO userStatusInfoDTO
 ) {
-    public static GetProfileStatusRes from (Boolean bool) {
-        return new GetProfileStatusRes(bool);
+    public static GetProfileStatusRes from (UserStatusInfoDTO userStatusInfoDTO) {
+        return new GetProfileStatusRes(userStatusInfoDTO);
     }
 }
