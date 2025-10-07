d3d5b61676f243b31217818f02b3629c02c71812
xxbeann <yjb4945@gmail.com>
2025-10-08 03:38:12 +0900

    feat: validateRange에 NPE 예외처리

--- src/main/java/com/econo_4factorial/newproject/user/domain/vo/PhysicalInfo.java
+++ src/main/java/com/econo_4factorial/newproject/user/domain/vo/PhysicalInfo.java
@@ -49,6 +49,7 @@ public class PhysicalInfo {
     }
 
     private static void validateRange(Long value, Long min, Long max, String msg) {
+        if (value == null) return;
         if (value < min || value > max) throw new IllegalArgumentException(msg);
     }
 
