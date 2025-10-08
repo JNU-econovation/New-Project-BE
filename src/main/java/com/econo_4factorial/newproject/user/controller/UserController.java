110af7781491d2a90eec82dd193e62b0a9680f0f
xxbeann <yjb4945@gmail.com>
2025-10-08 21:15:22 +0900

    chore: swagger description 편집

--- src/main/java/com/econo_4factorial/newproject/user/controller/UserController.java
+++ src/main/java/com/econo_4factorial/newproject/user/controller/UserController.java
@@ -13,7 +13,6 @@ import com.econo_4factorial.newproject.user.dto.ProfileStatusInfoDTO;
 import com.econo_4factorial.newproject.user.dto.req.AddPersonalInformationReq;
 import com.econo_4factorial.newproject.user.dto.req.AlertSettingReq;
 import com.econo_4factorial.newproject.user.dto.req.CheckNicknameReq;
-import com.econo_4factorial.newproject.user.dto.req.ProfileImageUrlReq;
 import com.econo_4factorial.newproject.user.dto.res.GetAlertSettingRes;
 import com.econo_4factorial.newproject.user.dto.res.GetNicknameAvailabilityRes;
 import com.econo_4factorial.newproject.user.dto.res.GetProfileRes;
@@ -117,16 +116,6 @@ public class UserController {
             @RequestBody @Valid AlertSettingReq alertSettingReq
     ) {
         userService.updateAlertSetting(userId, alertSettingReq);
-        return ApiResponse.success(null, HttpStatus.OK);
-    }
-
-    @PatchMapping("/profile/image")
-    @Operation(summary = "프로필 사진 업데이트", description = "프로필 사진을 업데이트합니다.")
-    public ApiResult<ApiResult.SuccessBody<Void>> updateProfileImage (
-            @UserId Long userId,
-            @RequestBody @Valid ProfileImageUrlReq profileImageUrlReq
-    ) {
-        userService.updateUserProfileImageName(userId, profileImageUrlReq.imageUrl());
         return ApiResponse.success(null, HttpStatus.OK);
     }
 
