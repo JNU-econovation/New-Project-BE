3ed6105323f9402257dbea75e4ee58997a47a74a
Woojae-Jeong <woo6388@naver.com>
2025-10-25 15:16:01 +0900

    feat: 산행 기록 상세조회 API 구현

--- src/main/java/com/econo_4factorial/newproject/travel/controller/TravelRecordController.java
+++ src/main/java/com/econo_4factorial/newproject/travel/controller/TravelRecordController.java
@@ -4,17 +4,16 @@ import com.econo_4factorial.newproject.common.annotation.UserId;
 import com.econo_4factorial.newproject.common.util.api.ApiResponse;
 import com.econo_4factorial.newproject.common.util.api.ApiResult;
 import com.econo_4factorial.newproject.travel.dto.TravelRecordDTO;
+import com.econo_4factorial.newproject.travel.dto.TravelRecordDetailDTO;
 import com.econo_4factorial.newproject.travel.dto.res.GetTravelRecordByMonthRes;
+import com.econo_4factorial.newproject.travel.dto.res.GetTravelRecordDetailRes;
 import com.econo_4factorial.newproject.travel.service.TravelRecordService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -30,12 +29,27 @@ public class TravelRecordController {
     public ApiResult<ApiResult.SuccessBody<GetTravelRecordByMonthRes>> getPathwaysOfCourse(
             @Parameter(name = "userId", description = "사용자 ID", required = true)
             @UserId Long userId,
+
             @Parameter(name = "year", description = "년(year)", required = true)
             @RequestParam("year") Integer year,
+
             @Parameter(name = "month", description = "월(month)", required = true)
             @RequestParam("month") Integer month
     ) {
         List<TravelRecordDTO> recordList = travelRecordService.findRecordByMonth(userId, year, month);
         return ApiResponse.success(GetTravelRecordByMonthRes.from(recordList), HttpStatus.OK);
+    }
+
+    @GetMapping("/{recordId}/details")
+    @Operation(summary = "산행 기록 상세조회", description = "특정 산행 기록을 상세조회 합니다")
+    public ApiResult<ApiResult.SuccessBody<GetTravelRecordDetailRes>> getPathwaysOfCourse(
+            @Parameter(name = "userId", description = "사용자 ID", required = true)
+            @UserId Long userId,
+
+            @Parameter(name = "recordId", description = "산행 기록 ID", required = true)
+            @PathVariable Long recordId
+    ) {
+        TravelRecordDetailDTO recordDetail = travelRecordService.findRecordById(recordId);
+        return ApiResponse.success(GetTravelRecordDetailRes.from(recordDetail), HttpStatus.OK);
     }
 }
