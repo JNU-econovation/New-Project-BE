1697e7b98e51e117a208c143c950c5fb06ac71bc
Woojae-Jeong <woo6388@naver.com>
2025-10-25 15:00:46 +0900

    feat: 산행 기록 상세 조회 DTO 구현

---
+++ src/main/java/com/econo_4factorial/newproject/travel/dto/TravelRecordDetailDTO.java
@@ -0,0 +1,46 @@
+package com.econo_4factorial.newproject.travel.dto;
+
+import com.econo_4factorial.newproject.travel.domain.TravelRecord;
+import org.locationtech.jts.geom.LineString;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.util.List;
+import java.util.stream.Stream;
+
+public record TravelRecordDetailDTO (
+        Long recordId,
+        String displayName,
+        Long staredAt,
+        Long endAt,
+        Long duration,
+        Double length,
+        List<List<BigDecimal>> coordinates,
+        Long courseId
+) {
+    public static TravelRecordDetailDTO from(TravelRecord travelRecord) {
+        Long staredAt = Timestamp.valueOf(travelRecord.getStartedAt()).getTime();
+        Long endAt = Timestamp.valueOf(travelRecord.getStartedAt()).getTime();
+        List<List<BigDecimal>> coordinates = convertCoordinatesFromLineString(travelRecord.getPaths());
+
+        return new TravelRecordDetailDTO(
+                travelRecord.getId(),
+                travelRecord.getDisplayName(),
+                staredAt,
+                endAt,
+                travelRecord.getTotalTravelTime().toMillis(),
+                travelRecord.getTotalTravelDistanceKm(),
+                coordinates,
+                travelRecord.getCourse().getId()
+        );
+    }
+
+    private static List<List<BigDecimal>> convertCoordinatesFromLineString(LineString coordinates) {
+        return Stream.of(coordinates.getCoordinates())
+                .map(coordinate -> List.of(
+                        BigDecimal.valueOf(coordinate.getX()),
+                        BigDecimal.valueOf(coordinate.getY())
+                ))
+                .toList();
+    }
+}
