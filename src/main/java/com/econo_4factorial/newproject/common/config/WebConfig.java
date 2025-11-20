5d08f2162972f45ed09401cde7e22ae4c2a2c70e
xxbeann <yjb4945@gmail.com>
2025-11-20 21:21:43 +0900

    Chore: 서버 환경과 CORS 동기화

--- src/main/java/com/econo_4factorial/newproject/common/config/WebConfig.java
+++ src/main/java/com/econo_4factorial/newproject/common/config/WebConfig.java
@@ -27,12 +27,13 @@ public class WebConfig implements WebMvcConfigurer {
                         "http://192.168.0.7:3000", // dding
                         "http://10.20.10.176:3000", // JNU
                         "http://10.20.10.217:3000", // JNU
+                        "http://10.30.131.71.3000", // JNU
                         "http://192.168.0.240:3000", // JNU
+                        "http://192.168.0.21:3000",  // JNU
                         "https://soop.euichan.com", // 프론트 배포 주소
                         "https://econo.soop.euichan.com:17779",// 동방
                         "http://192.168.0.100:3000", // Econo 5G 라즈베리파이 프론트
-                        "https://soop.euichan.com/", // 프론트 배포 주소
-                        "http://10.30.131.71:3000/"
+                        "https://soop.euichan.com/" // 프론트 배포 주소
                         )
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
