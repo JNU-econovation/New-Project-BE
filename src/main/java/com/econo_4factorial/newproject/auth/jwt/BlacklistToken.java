bcf20afa3154eef0726eee2141eb0eff83bc385b
xxbeann <yjb4945@gmail.com>
2026-03-06 23:46:50 +0900

    feat: blacklist 관리 파일 추가

---
+++ src/main/java/com/econo_4factorial/newproject/auth/jwt/BlacklistToken.java
@@ -0,0 +1,22 @@
+package com.econo_4factorial.newproject.auth.jwt;
+
+import jakarta.persistence.Id;
+import lombok.*;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
+
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@RedisHash(value = "blacklist_token")
+public class BlacklistToken {
+
+    @Id
+    private String accessToken;
+
+    private String status;
+
+    @TimeToLive
+    private Long expirationTime;
+}
