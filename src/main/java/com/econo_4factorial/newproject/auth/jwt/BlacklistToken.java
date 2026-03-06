2f8c5fa45176b0cd2fd9d000c29a97b1fe2fc1ce
xxbeann <yjb4945@gmail.com>
2026-03-07 00:18:44 +0900

    fix: `@Id` 어노테이션의 패키지 경로수정

--- src/main/java/com/econo_4factorial/newproject/auth/jwt/BlacklistToken.java
+++ src/main/java/com/econo_4factorial/newproject/auth/jwt/BlacklistToken.java
@@ -1,6 +1,6 @@
 package com.econo_4factorial.newproject.auth.jwt;
 
-import jakarta.persistence.Id;
+import org.springframework.data.annotation.Id;
 import lombok.*;
 import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.redis.core.TimeToLive;
