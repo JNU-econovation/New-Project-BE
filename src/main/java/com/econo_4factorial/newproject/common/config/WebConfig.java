a0fdb8ac242cce98a4a6309ec26abe39e8adf6e3
xxbeann <yjb4945@gmail.com>
2026-03-10 21:37:05 +0900

    fix: allowedOrigin 제거 및 변경

--- src/main/java/com/econo_4factorial/newproject/common/config/WebConfig.java
+++ src/main/java/com/econo_4factorial/newproject/common/config/WebConfig.java
@@ -22,19 +22,9 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOriginPatterns(
                         "https://localhost:3000", // local
                         "http://localhost:3000",
-                        "http://172.30.1.15:3000", // KT Giga Wifi
-                        "http://192.168.0.27:3000", // Econo 5G
-                        "http://192.168.0.7:3000", // dding
-                        "http://10.20.10.176:3000", // JNU
-                        "http://10.20.10.217:3000", // JNU
-                        "http://10.30.131.71.3000", // JNU
-                        "http://192.168.0.240:3000", // JNU
-                        "http://192.168.0.21:3000",  // JNU
-                        "https://soop.euichan.com", // 프론트 배포 주소
-                        "https://econo.soop.euichan.com:17779",// 동방
-                        "http://192.168.0.100:3000", // Econo 5G 라즈베리파이 프론트
-                        "https://soop.euichan.com/" // 프론트 배포 주소
-                        )
+                        "https://soop.euichan.com", // Frontend
+                        "https://api.econo.soop.euichan.com" // Backend
+                )
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
